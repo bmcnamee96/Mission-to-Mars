c3c501951e8afc54c80ab4fd7396206ddd571197
bmcnamee96 <bmac965@gmail.com>
2022-05-01 15:45:40 -0400

    created a working website

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # import dependencies
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+import scraping
 
 app = Flask(__name__)
 
@@ -21,4 +22,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
